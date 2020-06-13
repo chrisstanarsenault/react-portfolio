0221c89f4d9399309b03628f1403a736217949a2
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-06-13 00:57:45 -0400

    changed NavBar.js from styled-components to scss

--- src/components/main-section/NavBar.js
+++ src/components/main-section/NavBar.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Link } from 'react-scroll';
 
-import { bosYellow, mustardYellow } from '../../util/sharedStyles';
+import '../../styles/main-section/NavBar.scss';
 
 export default function NavBar() {
   return (
     <div>
-      <NavContainer>
+      <ul className="nav-bar">
         <Link
           to="about"
           spy={true}
@@ -15,7 +14,7 @@ export default function NavBar() {
           smooth={true}
           activeClass="active"
         >
-          <NavItem>About</NavItem>
+          <li className="nav-item">About</li>
         </Link>
         <Link
           to="projects"
@@ -24,7 +23,7 @@ export default function NavBar() {
           smooth={true}
           activeClass="active"
         >
-          <NavItem>Projects</NavItem>
+          <li className="nav-item">Projects</li>
         </Link>
         <Link
           to="contact"
@@ -33,55 +32,9 @@ export default function NavBar() {
           smooth={true}
           activeClass="active"
         >
-          <NavItem>Contact</NavItem>
+          <li className="nav-item">Contact</li>
         </Link>
-      </NavContainer>
+      </ul>
     </div>
   );
 }
-
-const NavContainer = styled.ul`
-  display: flex;
-  justify-content: flex-end;
-  list-style-type: none;
-`;
-
-const NavItem = styled.li`
-  padding: 10px 20px;
-  position: relative;
-  text-decoration: none;
-  color: ${(props) => (props.active ? mustardYellow : bosYellow)};
-  transition: all 0.3s ease-in;
-  font-size: 1.5rem;
-  font-weight: bold;
-
-  :before {
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 1px;
-    bottom: 0;
-    left: 0;
-    background-color: ${bosYellow};
-    visibility: hidden;
-    -webkit-transform: scaleX(0);
-    transform: scaleX(0);
-    -webkit-transition: all 0.5s cubic-bezier(1, 0.25, 0, 0.75) 0s;
-    transition: all 0.5s cubic-bezier(1, 0.25, 0, 0.75) 0s;
-  }
-
-  :hover:before {
-    visibility: visible;
-    -webkit-transform: scaleX(1);
-    transform: scaleX(1);
-  }
-
-  :hover {
-    color: ${bosYellow};
-    font-weight: bold;
-  }
-`;
-
-const Links = styled.a`
-  text-decoration: none;
-`;
