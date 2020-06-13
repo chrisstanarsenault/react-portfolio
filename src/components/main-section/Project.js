e7ac50563f9b0836d649f415c865748be9aba374
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-06-13 02:54:14 -0400

    changed Projects.js and Project.js from styled-components to scss

--- src/components/main-section/Project.js
+++ src/components/main-section/Project.js
@@ -1,105 +1,53 @@
 import React, { Fragment } from 'react';
-import styled from 'styled-components';
 
-import { blueGreen } from '../../util/sharedStyles';
-
+import '../../styles/main-section/Project.scss';
 import { projectImages, projectDevIcons } from '../../util/assets';
 
 export default function Project(props) {
   return (
     <Fragment>
       {props.projects.map((project, index) => (
-        <Container key={`container: ` + index}>
+        <div className="project-container" key={`container: ` + index}>
           <h3>{project.name}</h3>
-          <ProjectContainer image={projectImages[project.image]}>
-            <TechStackIconsContainer>
+          <div
+            className="project-image-container"
+            image={projectImages[project.image]}
+            style={{
+              background: `url(${projectImages[project.image]})`,
+              backgroundSize: 'cover',
+            }}
+          >
+            <div className="tech-stack-container">
               {project.devIcons.map((icon) => (
-                <DevIcons key={icon} className={projectDevIcons[icon]} />
+                <i key={icon} className={projectDevIcons[icon]} />
               ))}
-            </TechStackIconsContainer>
-            <ButtonsContainer>
+            </div>
+            <div className="buttons-container">
               {project.githubLink !== 'none' && (
                 <a href={project.githubLink}>
-                  <Buttons>
-                    <ButtonIcons className="devicon-github-plain"></ButtonIcons>
+                  <button>
+                    <i className="devicon-github-plain"></i>
                     Github
-                  </Buttons>
+                  </button>
                 </a>
               )}
               {project.liveLink !== 'none' && (
                 <a href={project.liveLink}>
-                  <Buttons>
-                    <ButtonIcons className="fas fa-home"></ButtonIcons>See Live
-                  </Buttons>
+                  <button>
+                    <i className="fas fa-home"></i>See Live
+                  </button>
                 </a>
               )}
-            </ButtonsContainer>
-          </ProjectContainer>
+            </div>
+          </div>
           <p>{project.info}</p>
           {project.isDesktopOnly && (
-            <DesktopOnlyText>
+            <p className="desktop-only-text">
               ** DESKTOP ONLY!! Mobile version in development! **
-            </DesktopOnlyText>
+            </p>
           )}
-        </Container>
+        </div>
       ))}
     </Fragment>
   );
 }
-
-const Container = styled.div`
-  border: 1px solid black;
-  margin-bottom: 20px;
-`;
-
-const ProjectContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 200px;
-  background: url(${(props) => props.image});
-  background-size: cover;
-`;
-
-const TechStackIconsContainer = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-  margin-right: 20px;
-  margin-top: 10px;
-  display: inline-flex;
-  background-color: #444444c7;
-`;
-
-const DevIcons = styled.i`
-  font-size: 30px;
-  color: white;
-  padding: 5px 10px;
-`;
-
-const ButtonIcons = styled.i`
-  font-size: 15px;
-  margin-right: 10px;
-`;
-
-const ButtonsContainer = styled.div`
-  position: absolute;
-  display: inline;
-  bottom: 0;
-  left: 0;
-`;
-
-const Buttons = styled.button`
-  background-color: ${blueGreen};
-  color: white;
-  border: none;
-  font-size: 20px;
-  padding: 5px 10px;
-  margin: 10px;
-  border-radius: 10px;
-  cursor: pointer;
-  font-weight: bold;
-`;
-
-const DesktopOnlyText = styled.p`
-  font-weight: bold;
-`;
